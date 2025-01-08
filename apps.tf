25fddb3b4a79af231cc9c8c022ad8009d212d330
Gabegi <gabriel_pirastru@hotmail.fr>
2025-01-08 14:48:21 +0100

    added delegation

--- apps.tf
+++ apps.tf
@@ -1,41 +1,49 @@
-# # Storage account for Function Apps
-# resource "azurerm_storage_account" "sa" {
-#   name                     = "examplestoragename"
-#   resource_group_name      = azurerm_resource_group.rg.name
-#   location                 = azurerm_resource_group.rg.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-# }
+# Storage account for Function Apps
+resource "azurerm_storage_account" "sa1" {
+  name                     = "dnsexamplesa"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
-# # App Service Plan for Function Apps
-# resource "azurerm_app_service_plan" "plan" {
-#   name                = "example-plan"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   sku {
-#     tier = "Dynamic"
-#     size = "Y1"
-#   }
-# }
+# App Service Plan for Function Apps
+resource "azurerm_service_plan" "asp" {
+  name                = "dns-asp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
 
-# # Function App 1
-# resource "azurerm_function_app" "function1" {
-#   name                       = "example-function1"
-#   location                   = azurerm_resource_group.rg.location
-#   resource_group_name        = azurerm_resource_group.rg.name
-#   app_service_plan_id        = azurerm_app_service_plan.plan.id
-#   storage_account_name       = azurerm_storage_account.sa.name
-#   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
-# #   subnet_id                  = azurerm_subnet.subnet1.id
-# }
+# Function App 1
+resource "azurerm_linux_function_app" "app1" {
+  name                = "dns-app1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
-# # Function App 2
-# resource "azurerm_function_app" "function2" {
-#   name                       = "example-function2"
-#   location                   = azurerm_resource_group.rg.location
-#   resource_group_name        = azurerm_resource_group.rg.name
-#   app_service_plan_id        = azurerm_app_service_plan.plan.id
-#   storage_account_name       = azurerm_storage_account.sa.name
-#   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
-# #   subnet_id                  = azurerm_subnet.subnet2.id
-# }
+  storage_account_name       = azurerm_storage_account.sa1.name
+  storage_account_access_key = azurerm_storage_account.sa1.primary_access_key
+  service_plan_id            = azurerm_service_plan.asp.id
+
+  virtual_network_subnet_id = azurerm_subnet.subnet1.id
+
+  site_config {}
+}
+
+
+# Function App 2
+resource "azurerm_linux_function_app" "app2" {
+  name                = "dns-app2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  storage_account_name       = azurerm_storage_account.sa1.name
+  storage_account_access_key = azurerm_storage_account.sa1.primary_access_key
+  service_plan_id            = azurerm_service_plan.asp.id
+
+    virtual_network_subnet_id = azurerm_subnet.subnet2.id
+
+
+  site_config {}
+}
--- networking.tf
+++ networking.tf
@@ -10,6 +10,15 @@ resource "azurerm_subnet" "subnet1" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
 }
 
 resource "azurerm_subnet" "subnet2" {
@@ -17,6 +26,15 @@ resource "azurerm_subnet" "subnet2" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
 }
 
 # # Private Endpoint for Function App 1
