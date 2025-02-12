5768e8277727155c17c68011dd4a1586910f22ce
Gabegi <gabriel_pirastru@hotmail.fr>
2025-02-12 12:51:34 +0100

    ip and fqdn matching automatically

--- dns.tf
+++ dns.tf
@@ -8,4 +8,5 @@ resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
   resource_group_name   = azurerm_resource_group.rg.name
   private_dns_zone_name = azurerm_private_dns_zone.private_dns.name
   virtual_network_id    = azurerm_virtual_network.vnet.id
+  registration_enabled = true
 }
