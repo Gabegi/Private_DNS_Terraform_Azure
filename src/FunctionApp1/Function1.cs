fbbad8b8af58a418e3c66f64b4927dd9bfca1026
Gabegi <gabriel_pirastru@hotmail.fr>
2025-02-12 13:12:54 +0100

    adding functions for testing

---
+++ src/FunctionApp1/Function1.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+public static class CallFunctionApp
+{
+    private static readonly HttpClient httpClient = new HttpClient();
+
+    [Function("CallFunctionApp2")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req,
+        ILogger log)
+    {
+        log.LogInformation("Triggered Function App 1. Calling Function App 2...");
+
+        string functionApp2Url = "https://dns-app2.azurewebsites.net/api/Function2"; // Update with the correct function name
+
+        try
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(functionApp2Url);
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            log.LogInformation($"Response from Function App 2: {responseBody}");
+            return new OkObjectResult($"Function App 2 responded with: {responseBody}");
+        }
+        catch (HttpRequestException ex)
+        {
+            log.LogError($"Error calling Function App 2: {ex.Message}");
+            return new StatusCodeResult(500);
+        }
+    }
+}
---
+++ src/FunctionApp2/Function1.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Functions.Worker;
+
+public static class Function2
+{
+    [Function("Function2")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req,
+        ILogger log)
+    {
+        log.LogInformation("Function App 2 triggered.");
+        return new OkObjectResult("Hello from Function App 2!");
+    }
+}
