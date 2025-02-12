d50e62d71924ebcc78c06d306ffabc045f0812b1
Gabegi <gabriel_pirastru@hotmail.fr>
2025-02-12 11:19:03 +0100

    subnet  for pe app2

--- pe.tf
+++ pe.tf
@@ -11,3 +11,17 @@ resource "azurerm_private_endpoint" "app1_pe" {
     is_manual_connection           = false
   }
 }
+
+resource "azurerm_private_endpoint" "app2_pe" {
+  name                = "app2-pe"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.subnet3.id
+
+  private_service_connection {
+    name                           = "app2-privateserviceconnection"
+    private_connection_resource_id = azurerm_linux_function_app.app2.id
+    subresource_names              = ["sites"]
+    is_manual_connection           = false
+  }
+}
