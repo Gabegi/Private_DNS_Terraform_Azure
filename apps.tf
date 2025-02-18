a61d1c1c75de2fbbbcceb95f776fb7d469cdb842
Gabegi <gabriel_pirastru@hotmail.fr>
2025-02-18 11:13:49 +0100

    building apps

--- apps.tf
+++ apps.tf
@@ -12,7 +12,7 @@ resource "azurerm_service_plan" "asp" {
   name                = "dns-asp"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  os_type             = "Linux"
+  os_type             = "Windows"
   sku_name            = "P1v2"
 }
 
