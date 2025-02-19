d0c475dde692c52faed46a4be5c4518ad6dbe24a
Gabegi <gabriel_pirastru@hotmail.fr>
2025-02-19 14:47:34 +0100

    app working

--- src/Application_1/Function1.cs
+++ src/Application_1/Function1.cs
@@ -11,7 +11,8 @@ public static class CallFunc
     public static async Task<IActionResult> Run(
         [HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
-        string functionApp2Url = "https://dns-app2.azurewebsites.net/api/ReceiveCall"; 
+        string functionApp2Url = "https://dns-app2.azurewebsites.net/api/ReceiveCall?code=xxxx";
+        // "http://localhost:7218/api/ReceiveCall";
 
         try
         {
