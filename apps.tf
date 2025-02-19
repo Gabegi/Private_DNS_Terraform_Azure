bfdc9909a324c40c9c95207f7f4b962dffe81cc9
Gabegi <gabriel_pirastru@hotmail.fr>
2025-02-19 11:51:43 +0100

    adding cors

--- apps.tf
+++ apps.tf
@@ -61,5 +61,12 @@ resource "azurerm_windows_function_app" "app2" {
     application_stack {
       dotnet_version = "v8.0"
     }
+
+    cors {
+      allowed_origins = [
+        "https://portal.azure.com"
+      ]
+      support_credentials = true
+    }
 }
 }
