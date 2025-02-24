0ca6633b2d086eaf481a68fbc60db9dc90654695
Gabegi <gabriel_pirastru@hotmail.fr>
2025-02-24 12:11:00 +0100

    adding nsgs

--- nsg.tf
+++ nsg.tf
@@ -1,21 +1,52 @@
-resource "azurerm_network_security_group" "example" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-sub2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-  security_rule {
-    name                       = "test123"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+# Associate NSG with PE Subnet
+resource "azurerm_subnet_network_security_group_association" "assoc_app2_sub" {
+  subnet_id                 = azurerm_subnet.subnet2.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
 
-  tags = {
-    environment = "Production"
-  }
+resource "azurerm_network_security_rule" "nsg-rule-1" {
+  name                        = "deny-all"
+  priority                    = 4096  # Lowest priority, so it applies last
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_group" "nsg-2" {
+  name                = "nsg-sub3"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Associate NSG with PE Subnet
+resource "azurerm_subnet_network_security_group_association" "assoc_pe_sub" {
+  subnet_id                 = azurerm_subnet.subnet3.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_network_security_rule" "nsg-rule-2" {
+  name                        = "deny-all"
+  priority                    = 4096  # Lowest priority, so it applies last
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+
+  resource_group_name         = "*"
+  destination_address_prefix  = azurerm_private_endpoint.app2_pe.private_service_connection[0].private_ip_address
+  network_security_group_name = azurerm_network_security_group.nsg-2.name
 }
