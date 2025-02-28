911e2951959f5bd1fe48374649d70f0db6d75d2c
Gabegi <gabriel_pirastru@hotmail.fr>
2025-02-28 12:00:34 +0100

    nsg

--- nsg.tf
+++ nsg.tf
@@ -167,3 +167,18 @@ resource "azurerm_network_security_rule" "nsg-1-rule-allow-sub1-tosub3-outbound"
   destination_address_prefix  = azurerm_subnet.subnet3.address_prefixes[0]  # App2 subnet
   
 }
+
+resource "azurerm_network_security_rule" "allow-sub1-to-sub2-outbound" {
+  name                        = "allow-sub1-to-sub2"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  network_security_group_name = azurerm_network_security_group.nsg-1.name
+  resource_group_name         = azurerm_resource_group.rg.name
+
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = azurerm_subnet.subnet1.address_prefixes[0]  # App1 Subnet
+  destination_address_prefix  = azurerm_subnet.subnet2.address_prefixes[0]  # App2 Subnet
+}
