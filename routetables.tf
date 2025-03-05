bbc58265371a8206b6996de04d88a2853f69b999
Gabegi <gabriel_pirastru@hotmail.fr>
2025-03-05 15:00:37 +0100

    route tables

--- routetables.tf
+++ routetables.tf
@@ -1,31 +1,41 @@
-# # resource "azurerm_route_table" "block_internet" {
-# #   name                = "block-internet-rt"
-# #   location            = azurerm_resource_group.rg.location
-# #   resource_group_name = azurerm_resource_group.rg.name
-
-# #   route {
-# #     name           = "block-all-outbound"
-# #     address_prefix = "0.0.0.0/0"
-# #     next_hop_type  = "None"  # Blackhole traffic
-# #   }
-# # }
-
-# # resource "azurerm_subnet_route_table_association" "subnet2-block" {
-# #   subnet_id      = azurerm_subnet.subnet2.id  # The subnet hosting your Function App
-# #   route_table_id = azurerm_route_table.block_internet.id
-# # }
-
-# resource "azurerm_route_table" "app_route" {
-#   name                = "app-route"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
+# resource "azurerm_route_table" "subnet2_route_table" {
+#   name                          = "subnet2-route-table"
+#   location                      = azurerm_resource_group.rg.location
+#   resource_group_name           = azurerm_resource_group.rg.name
 # }
 
-# resource "azurerm_route" "app1_to_app2" {
-#   name                = "route-app1-to-app2"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   route_table_name    = azurerm_route_table.app_route.name
-#   address_prefix      = azurerm_subnet.subnet2.address_prefixes[0] # App2 subnet
-#   next_hop_type       = "VirtualAppliance"
-#   next_hop_in_ip_address = "10.0.2.4" # If using Azure Firewall or a NAT Gateway
+# # Block ALL outbound internet traffic
+# resource "azurerm_route" "block_internet" {
+#   name                    = "block-internet"
+#   resource_group_name     = azurerm_resource_group.rg.name
+#   route_table_name        = azurerm_route_table.subnet2_route_table.name
+#   address_prefix          = "0.0.0.0/0"  # Match all internet traffic
+#   next_hop_type           = "None"       # DROP all internet traffic
 # }
+
+# # Associate the Route Table with subnet2
+# resource "azurerm_subnet_route_table_association" "subnet2_route_assoc" {
+#   subnet_id      = azurerm_subnet.subnet2.id
+#   route_table_id = azurerm_route_table.subnet2_route_table.id
+# }
+
+resource "azurerm_route_table" "subnet3_route_table" {
+  name                          = "subnet3-route-table"
+  location                      = azurerm_resource_group.rg.location
+  resource_group_name           = azurerm_resource_group.rg.name
+}
+
+# Block ALL outbound internet traffic
+resource "azurerm_route" "block_internet_subnet3" {
+  name                    = "block-internet"
+  resource_group_name     = azurerm_resource_group.rg.name
+  route_table_name        = azurerm_route_table.subnet3_route_table.name
+  address_prefix          = "0.0.0.0/0"  # Match all internet traffic
+  next_hop_type           = "None"       # DROP all internet traffic
+}
+
+# Associate the Route Table with subnet3
+resource "azurerm_subnet_route_table_association" "subnet3_route_assoc" {
+  subnet_id      = azurerm_subnet.subnet3.id
+  route_table_id = azurerm_route_table.subnet3_route_table.id
+}
