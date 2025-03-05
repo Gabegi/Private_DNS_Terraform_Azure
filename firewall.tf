eb902606c3d73a6656582ee33924b112f63b9ad1
Gabegi <gabriel_pirastru@hotmail.fr>
2025-03-05 15:35:10 +0100

    firewall

--- firewall.tf
+++ firewall.tf
@@ -3,29 +3,36 @@ resource "azurerm_firewall" "fire-wall" {
   name                = "myFirewall"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-#   firewall_policy_id  = azurerm_firewall_policy.firewall-policy1.id
+  firewall_policy_id  = azurerm_firewall_policy.firewall-policy1.id
 
   sku_name            = "AZFW_VNet"
   sku_tier            = "Standard"
 }
 
-# # Azure Firewall Policy (This is where we define the blocking rules)
-# resource "azurerm_firewall_policy" "firewall-policy1" {
-#   name                = "myFirewallPolicy"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_firewall_policy" "firewall-policy1" {
+  name                = "myFirewallPolicy"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
-#   rule_collection {
-#     name     = "block_external_traffic"
-#     action   = "Deny"
-#     priority = 100
+resource "azurerm_firewall_policy_rule_collection" "deny_external_to_subnet2" {
+  name                = "denyExternalTrafficToSubnet2"
+  priority            = 100
+  action              = "Deny"
+  rule_type           = "NetworkRule"
+  firewall_policy_id = azurerm_firewall_policy.firewall-policy1.id
 
-#     rule {
-#       name             = "block_external"
-#       protocol         = "Any"
-#       source_addresses = ["*"]  # Source addresses: any (external)
-#       destination_addresses = [azurerm_subnet.subnet2.address_prefixes[0]]  # Subnet2 destination
-#       destination_ports = ["*"]
-#     }
-#   }
-# }
+  rule {
+    name                    = "blockExternalToSubnet2"
+    source_addresses        = ["*"]  # Block all external traffic
+    destination_addresses   = [azurerm_subnet.subnet2.address_prefixes[0]]  # Destination subnet2
+    protocols               = ["Any"]  # Block all protocols (TCP, UDP, ICMP, etc.)
+    destination_ports       = ["*"]  # Block all ports
+  }
+}
+
+# Assign Firewall Policy to the Azure Firewall
+resource "azurerm_firewall_policy_association" "firewall-policy-association" {
+  firewall_policy_id = azurerm_firewall_policy.firewall-policy1.id
+  firewall_id        = azurerm_firewall.fire-wall.id
+}
