4e39a03f23763b02fd6c86ce1ff1bb62daa2a8c2
Gabegi <gabriel_pirastru@hotmail.fr>
2025-03-06 12:09:22 +0100

    testing

--- nsg.tf
+++ nsg.tf
@@ -87,7 +87,7 @@ resource "azurerm_subnet_network_security_group_association" "assoc_pe_sub" {
 
 //////////////////////// Subnet 1 NSG //////////////////////////////////////
 resource "azurerm_network_security_group" "nsg-1" {
-  name                = "nsg-1"
+  name                = "nsg-sub1"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
