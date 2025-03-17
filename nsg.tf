1299eb56cede84d63c4f30a149388f869f6a0ee9
Gabegi <gabriel_pirastru@hotmail.fr>
2025-03-17 10:29:53 +0100

    nsg

--- nsg.tf
+++ nsg.tf
@@ -49,31 +49,31 @@ resource "azurerm_subnet_network_security_group_association" "subnet2-nsg2" {
   network_security_group_id = azurerm_network_security_group.nsg-sub2.id
 }
 
-# //////////////////////// Subnet 3 NSG //////////////////////////////////////
-# resource "azurerm_network_security_group" "nsg-2" {
-#   name                = "nsg-sub3"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
+//////////////////////// Subnet 3 NSG //////////////////////////////////////
+resource "azurerm_network_security_group" "nsg-2" {
+  name                = "nsg-sub3"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-#    # Block ALL inbound traffic
-#   security_rule {
-#     name                       = "deny-all-inbound"
-#     priority                   = 100
-#     direction                  = "Inbound"
-#     access                     = "Deny"
-#     protocol                   = "*"
-#     source_port_range          = "*"
-#     destination_port_range     = "*"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
-# }
+   # Block ALL inbound traffic
+  security_rule {
+    name                       = "deny-all-inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
 
-# # Associate NSG with PE Subnet
-# resource "azurerm_subnet_network_security_group_association" "assoc_pe_sub" {
-#   subnet_id                 = azurerm_subnet.subnet3.id
-#   network_security_group_id = azurerm_network_security_group.nsg-2.id
-# }
+# Associate NSG with subnet3 Sbnet
+resource "azurerm_subnet_network_security_group_association" "assoc_pe_sub" {
+  subnet_id                 = azurerm_subnet.subnet3.id
+  network_security_group_id = azurerm_network_security_group.nsg-2.id
+}
 
 # //////////////////////// Subnet 1 NSG //////////////////////////////////////
 # resource "azurerm_network_security_group" "nsg-1" {
