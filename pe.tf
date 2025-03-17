453dfb1661e03d33392c36dd64cf9044e4e428cd
Gabegi <gabriel_pirastru@hotmail.fr>
2025-03-17 12:23:54 +0100

    app3 networking

--- pe.tf
+++ pe.tf
@@ -35,3 +35,22 @@ resource "azurerm_private_endpoint" "app2_pe" {
     private_dns_zone_ids = [azurerm_private_dns_zone.private_dns.id]
   }
 }
+
+resource "azurerm_private_endpoint" "app3_pe" {
+  name                = "app3-pe"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.subnet3.id
+
+  private_service_connection {
+    name                           = "app2-privateserviceconnection"
+    private_connection_resource_id = azurerm_windows_function_app.app3.id
+    subresource_names              = ["sites"]
+    is_manual_connection           = false
+  }
+
+    private_dns_zone_group {
+    name                 = "example-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.private_dns.id]
+  }
+}
