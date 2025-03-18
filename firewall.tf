1bbb11a72d0185df86ed07306d3b4f7677aeb60b
Gabegi <gabriel_pirastru@hotmail.fr>
2025-03-18 14:22:46 +0100

    firewall

--- firewall.tf
+++ firewall.tf
@@ -6,3 +6,19 @@ resource "azurerm_public_ip" "fw_pip" {
   allocation_method   = "Static"
   sku                = "Standard"
 }
+
+# Azure Firewall
+resource "azurerm_firewall" "fw" {
+  name                = var.firewall_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.subnet_firewall.id
+    private_ip_address   = var.firewall_private_ip
+    public_ip_address_id = azurerm_public_ip.fw_pip.id
+  }
+}
