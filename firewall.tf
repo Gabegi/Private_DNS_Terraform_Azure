6a258d8e16d2da2b9d24711703cecf434d1f372f
Gabegi <gabriel_pirastru@hotmail.fr>
2025-03-18 14:23:24 +0100

    firewall

--- firewall.tf
+++ firewall.tf
@@ -29,3 +29,13 @@ resource "azurerm_route_table" "rt" {
   location                      = azurerm_resource_group.rg.location
   resource_group_name           = azurerm_resource_group.rg.name
 }
+
+# Route to Redirect All Traffic to Firewall
+resource "azurerm_route" "default_route" {
+  name                   = "route-to-firewall"
+  resource_group_name    = azurerm_resource_group.rg.name
+  route_table_name       = azurerm_route_table.rt.name
+  address_prefix         = "0.0.0.0/0"
+  next_hop_type          = "VirtualAppliance"
+  next_hop_in_ip_address = var.firewall_private_ip
+}
