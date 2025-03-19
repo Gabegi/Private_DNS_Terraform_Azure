c0f949bfc4e88008895c404d88b31a393d7b438c
Gabegi <gabriel_pirastru@hotmail.fr>
2025-03-19 14:02:33 +0100

    gateway

--- firewall.tf
+++ firewall.tf
@@ -7,21 +7,21 @@ resource "azurerm_public_ip" "fw_pip" {
   sku                = "Standard"
 }
 
-# Azure Firewall
-resource "azurerm_firewall" "fw" {
-  name                = "dns-firewall"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "AZFW_VNet"
-  sku_tier            = "Standard"
+# # Azure Firewall
+# resource "azurerm_firewall" "fw" {
+#   name                = "dns-firewall"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   sku_name            = "AZFW_VNet"
+#   sku_tier            = "Standard"
 
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.subnet_firewall.id
-    private_ip_address   = var.firewall_private_ip
-    public_ip_address_id = azurerm_public_ip.fw_pip.id
-  }
-}
+#   ip_configuration {
+#     name                 = "configuration"
+#     subnet_id            = azurerm_subnet.subnet_firewall.id
+#     private_ip_address   = var.firewall_private_ip
+#     public_ip_address_id = azurerm_public_ip.fw_pip.id
+#   }
+# }
 
 # # Route Table for App Subnet to Force Traffic Through Firewall
 # resource "azurerm_route_table" "rt" {
