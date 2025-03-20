3e74d0a37c75b84830c4795271fd4fe527808626
Gabegi <gabriel_pirastru@hotmail.fr>
2025-03-20 09:29:26 +0100

    gateway

--- networking.tf
+++ networking.tf
@@ -71,4 +71,16 @@ resource "azurerm_subnet" "subnet5" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.5.0/24"]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name = "Microsoft.Network/virtualNetworks"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/action",
+        "Microsoft.Network/virtualNetworks/subnets/join/action"
+      ]
+    }
+  }
 }
