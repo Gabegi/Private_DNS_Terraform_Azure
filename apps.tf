b5e8c67c6d42a0458d2606ed6fad076d691cc94f
Gabegi <gabriel_pirastru@hotmail.fr>
2025-03-20 10:24:11 +0100

    gateway

--- apps.tf
+++ apps.tf
@@ -91,13 +91,6 @@ resource "azurerm_windows_function_app" "app2" {
 app_settings = {
   "APPLICATIONINSIGHTS_CONNECTION_STRING" = azurerm_application_insights.app_insights.connection_string
     "APPINSIGHTS_INSTRUMENTATIONKEY"        = azurerm_application_insights.app_insights.instrumentation_key
-  # "WEBSITE_USE_PLACEHOLDER_DOTNETISOLATED" = "1" // allows to target different versions of .Net
-  # "WEBSITE_RUN_FROM_PACKAGE"          = "1"
-  # "WEBSITE_VNET_ROUTE_ALL"            = "1" // Ensures all outbound traffic goes through VNet
-  # "WEBSITE_PRIVATE_ENDPOINT_ENABLED"  = "1" // Ensures Private Endpoint is enforced
-  # "WEBSITE_DNS_SERVER"                = "20.105.224.40"
-  # //"10.0.3.5"
-  # // "20.105.224.40" //"168.63.129.16" // Uses Azure's private DNS resolver
 }
 
 }
